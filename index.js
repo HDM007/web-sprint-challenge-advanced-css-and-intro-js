25ee528cece512cf84e2b734fddec789654cfa55
hdm007 <harry.mardirossian13@gmail.com>
2020-12-11 12:40:06 -0500

    removeArtist complete

--- index.js
+++ index.js
@@ -265,8 +265,9 @@ console.log(get20s(artists))
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(arr, index){
+   arr.splice(index, 1);
+   return arr.length;
 }
    
 
